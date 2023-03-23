0cda985805bcc1cde0983acd123c75f2422e1731
ltressen <paloouf@gmail.com>
2023-03-23 16:49:00 +0100

    updated 23/03

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 15:06:31 by ltressen          #+#    #+#             */
-/*   Updated: 2023/03/22 15:37:57 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/03/23 16:17:02 by ltressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,14 @@
 
 int	g_sig;
 
-static void	ft_resend(int pid, int signal)
-{
-	if (kill(pid, signal) == -1)
-	{
-		usleep(50);
-		ft_resend(pid, signal);
-	}
-}
-
 static void	ft_break(int signal)
 {
 	if (signal == SIGUSR1)
 		g_sig = 1;
 	else if (signal == SIGUSR2)
 	{
-		ft_printf("Message received :)");
-		g_sig = 1;
+		ft_printf("Message received :)\n");
+		exit(EXIT_SUCCESS);
 	}
 }
 
@@ -38,22 +29,19 @@ static void	ft_send(int pid, char c)
 {
 	int	i;
 
-	i = 10;
+	i = 8;
 	while (i >= 0)
 	{
 		if (c & (1 << i))
-		{
-			if (kill(pid, SIGUSR1) == -1)
-				ft_resend(pid, SIGUSR1);
-		}
+			kill(pid, SIGUSR1);
 		else
-		{
-			if (kill(pid, SIGUSR2) == -1)
-				ft_resend(pid, SIGUSR2);
-		}
+			kill(pid, SIGUSR2);
 		i--;
 		while (g_sig != 1)
+		{
 			signal(SIGUSR1, ft_break);
+			signal(SIGUSR2, ft_break);
+		}
 		usleep(5);
 		g_sig = 0;
 	}
@@ -90,8 +78,6 @@ int	main(int argc, char **argv)
 			i++;
 		}
 		ft_send(ft_atoi(argv[1]), '\0');
-		while (g_sig == 0)
-			signal(SIGUSR2, ft_break);
 	}
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 08:55:56 by ltressen          #+#    #+#             */
-/*   Updated: 2023/03/22 15:13:05 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/03/23 16:27:37 by ltressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 static void	ft_msg(int res, int i, siginfo_t *lol)
 {
-	if (res == 0 && i == 11)
+	if (res == 0 && i == 9)
 	{
-		usleep(500);
+		usleep(5);
 		kill(lol->si_pid, SIGUSR2);
 	}
 }
 
 static void	ft_write(int signal, siginfo_t *lol, void *nimp)
 {
-	static int	bits[11];
-	static int	i = 11;
+	static int	bits[9];
+	static int	i = 9;
 	int			res;
 
 	(void)nimp;
@@ -33,19 +33,20 @@ static void	ft_write(int signal, siginfo_t *lol, void *nimp)
 	else if (signal == SIGUSR2)
 		bits[i--] = 0;
 	res = 0;
-	kill(lol->si_pid, SIGUSR1);
 	if (i == 0)
 	{
-		while (i < 10)
+		while (i < 8)
 		{
-			res += bits[10 - i] * ft_power(2, 9 - i);
+			res += bits[8 - i] * ft_power(2, 7 - i);
 			i++;
 		}
-		i = 11;
+		i = 9;
 		ft_printf("%c", (char)res);
-		ft_bzero(bits, 11);
+		ft_bzero(bits, 9);
 	}
+	usleep(5);
 	ft_msg(res, i, lol);
+	kill(lol->si_pid, SIGUSR1);
 }
 
 int	main(void)
@@ -55,10 +56,9 @@ int	main(void)
 	siga.sa_sigaction = ft_write;
 	siga.sa_flags = SA_SIGINFO;
 	sigemptyset(&siga.sa_mask);
+	sigaction(SIGUSR1, &siga, 0);
+	sigaction(SIGUSR2, &siga, 0);
 	ft_printf("%d\n", getpid());
 	while (1)
-	{
-		sigaction(SIGUSR1, &siga, 0);
-		sigaction(SIGUSR2, &siga, 0);
-	}
+		sleep(1);
 }
